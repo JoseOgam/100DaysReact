cb673db772668aa060ff87a4d37052280b785663
JoseOgam <jotienoochieng@gmail.com>
2021-06-24 16:59:24 -0400

    use effect implementation

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,16 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const Form = (props) => {
     const [data, setData] = useState([])
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [phone, setPhone] = useState('')
 
-    
+    useEffect(() => {
+        var dataInfo = JSON.parse(localStorage.getItem('data'))
+        if (dataInfo){
+            setData(dataInfo)
+        }
+    },[])
 
+    
+    useEffect(() => {
+    localStorage.setItem('data', JSON.stringify(data))
+    }, [data])
+    
     const submit = (e)=>{
     e.preventDefault()
     setData([...data, {name,email,phone}])
-  }
+    }
     return (
         <div>
             <h4>{props.title}</h4>
